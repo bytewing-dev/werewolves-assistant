ad674a3059974a8dbb2d05c306ad9c6bce7a0c20
bytewing-dev <niightwiing59@gmail.com>
2025-04-06 17:03:02 +0200

    Imported new font Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  // Poids disponibles: 100, 200, 300, 400, 500, 600, 700, 800, 900
+  weight: ['400', '500', '600', '700'],
+  // Inclure uniquement le sous-ensemble latin
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  // Nom de la variable CSS
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
         {children}
       </body>
     </html>
