93d1c609e725b5e60c876c757263936736a8c0da
bytewing-dev <niightwiing59@gmail.com>
2025-04-06 17:13:24 +0200

    Minimiszed the use of native CSS for better Tailwind use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,7 @@ import "./globals.css";
 const poppins = Poppins({
   // Poids disponibles: 100, 200, 300, 400, 500, 600, 700, 800, 900
   weight: ["400", "500", "600", "700"],
-  // Inclure uniquement le sous-ensemble latin
   subsets: ["latin"],
-  // Nom de la variable CSS
   variable: "--font-poppins",
 });
 
@@ -23,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body
+        className={`${poppins.variable} bg-background text-foreground font-sans antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
